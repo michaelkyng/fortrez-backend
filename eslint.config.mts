ab7c3129cc95f866db8586ea617cedbffb44907f
Michael Akpuofoba <67930856+michaelkyng@users.noreply.github.com>
2025-09-10 14:20:48 +0100

    fix : Reconfigured for vercel

--- eslint.config.mts
+++ eslint.config.mts
@@ -24,7 +24,7 @@ export default defineConfig([
       },
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['off', { argsIgnorePattern: '^_' }],
     },
   },
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,12 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): object {
+    return {
+      message: 'Fortrez API is running!',
+      version: '1.0.0',
+      docs: '/api',
+    };
   }
 }
